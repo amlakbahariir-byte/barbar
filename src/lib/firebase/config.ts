7589ae6e0fd7d9bdf5066ba8f2230ec2b411e82f
Bahari Real State <amlakbahari.ir@gmail.com>
2025-09-19 16:25:55 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,18 +3,19 @@ import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 
 const firebaseConfig = {
-  "projectId": "studio-1766914539-f6cf7",
-  "appId": "1:929997868052:web:aee9050443c82fcaa76b01",
-  "apiKey": "AIzaSyCaig6O-U9o7xzi50L9NVmgH97XvoAfutI",
-  "authDomain": "studio-1766914539-f6cf7.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "929997868052"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
 let app: FirebaseApp;
 let auth: Auth;
 
+// This check prevents firebase from being initialized on the server side.
 if (typeof window !== 'undefined') {
   if (!getApps().length) {
     app = initializeApp(firebaseConfig);
